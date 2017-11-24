bb3f6df7d5ff249cce2f10e9798760c21500468a
lukexingdbmgr <lukexingdbmgr@gmail.com>
2017-11-24 15:58:16 -0800

    leaves

---
+++ leavesOfBT.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def findLeaves_help(self, root, res):
+        if root == None:
+            return
+
+        h = max(self.findLeaves_help(root.left, res), self.findLeaves_help(root.right, res))
+        if len(res) == h:
+            res.append([root.val])
+        else:
+            res[h].append(root.val)
+        return h + 1
+
+    def findLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.findLeaves_help(root, res)
+        return res
