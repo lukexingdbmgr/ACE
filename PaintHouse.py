fe7c864895ac2990a23c002d323102aae45acd01
lukexingdbmgr <lukexingdbmgr@gmail.com>
2017-11-24 11:52:16 -0800

    add comment
    fix cost[row][col]

--- PaintHouse.py
+++ PaintHouse.py
@@ -8,20 +8,29 @@ class Solution(object):
         if (len(cost) == 0 or len(cost[0]) == 0):
             return 0
 
-        for i in range(1, len(cost[0])):
-            '''
-            case 1 : paint use color 0:
-                    the sum from house[0] to house[i] when house [i]  use color 0 = cost of house[i] using color 0 + min(cost from house i to (i-1) use when house i -1 use color 1, cost from house i to (i-1) when house i - 1 use color 2)
+        L = len(cost)
 
-                    if house[i] use color 0 then the previous one can only use 1 or 2
+        for i in range(1, L):
+            '''
+            cost[row][col]
+                row : len(cost)
+                col : len(cost[0])
+                
+            for house i we try 3 possible paint solutios:
+                if we paint with color 0, 
+                    then the last house can only have two options: color 1 and color 2
+                    so the overall solution is min((i-1, 1), (i-1, 2)
+                    +
+                    cost of house[i] with color k (old)
             '''
             cost[i][0] = cost[i][0] + min(cost[i - 1][1], cost[i - 1][2])
             cost[i][1] = cost[i][1] + min(cost[i - 1][0], cost[i - 1][2])
             cost[i][2] = cost[i][2] + min(cost[i - 1][0], cost[i - 1][1])
 
-        l = len(cost[0])
-
-        return min(cost[l - 1][0], min(cost[l - 1][1], cost[l - 1][2]))
+        ## the last house have 3 options;
+        ## all of the painting options in 3 buckets
+        ## find the min of 3
+        return min(cost[L - 1][0], min(cost[L - 1][1], cost[L - 1][2]))
 
 
 if __name__ == "__main__":
