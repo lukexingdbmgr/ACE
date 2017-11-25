f901a22bea07c98c247162b3637211f98e9e8359
lukexingdbmgr <lukexingdbmgr@gmail.com>
2017-11-25 14:57:07 -0800

    max heap

---
+++ 716_max_stack.py
@@ -0,0 +1,79 @@
+## python throw exceptions
+
+
+from  collections import OrderedDict
+
+
+class MaxStack(object):
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.dict = OrderedDict()
+        self.k = 0
+        self.q = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        self.k += 1
+        self.dict[self.k] = x
+
+        if len(self.q) == 0:
+            ## value -> key
+            self.q.append((self.k, x))
+        else:
+            v, k = self.q[-1]
+            if x >= v:
+                self.q.append((self.k, x))
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        k, v = self.dict.popitem(last=True)
+        q_k, q_v = self.q[-1]
+
+        if k == q_k:
+            self.q.pop(-1)
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        (c, _) = self.dict.items()
+        return c
+
+    def peekMax(self):
+        """
+        :rtype: int
+        """
+        if len(self.q) == 0:
+            return
+        q_k, q_v = self.q[-1]
+        return q_v
+
+    def popMax(self):
+        """
+        :rtype: int
+        """
+        q_k, q_v = self.q[-1]
+        self.dict.pop(q_k)
+        self.q.pop()
+
+
+if __name__ == "__main__":
+    d = OrderedDict()
+    d[1] = 10
+    d[2] = 11
+    d[3] = 15
+    t = d.popitem(last=True)
+    print("pop = ", t)
+
+    (c, _) = d.items()
+
+    print(c)
+
+    print(d)
