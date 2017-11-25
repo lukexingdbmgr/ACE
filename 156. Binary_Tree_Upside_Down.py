876d64b37c3a0122d7b902532198dc6163cdfe30
lukexingdbmgr <lukexingdbmgr@gmail.com>
2017-11-25 01:24:26 -0800

    1. return prev; as this is the new root           as cur go to the root position and                   prev = cur 2. change the order

--- 156. Binary_Tree_Upside_Down.py
+++ 156. Binary_Tree_Upside_Down.py
@@ -4,6 +4,7 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+#         similar to 206. Reverse Linked List
 
 class Solution(object):
     def upsideDownBinaryTree(self, root):
@@ -17,12 +18,36 @@ class Solution(object):
         cur = root
         while (cur != None):
             next = cur.left  # save
-            cur.left = prev  # change
+            cur.left = lastRight  # change
+
             right = cur.right  # save
-            cur.right = lastRight  # change
+            cur.right = prev  # change
 
             ## state change
             prev = cur
             cur = next
             lastRight = right
         return prev
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+#       TODO: replay this
+
+class Solution2(object):
+    def upsideDownBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root or not root.left and not root.right: return root
+        newRoot = self.upsideDownBinaryTree(root.left)
+        root.left.left = root.right
+        root.left.right = root
+        root.left = None
+        root.right = None
+        return newRoot
