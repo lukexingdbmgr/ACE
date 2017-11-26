bebdccdcb112de4ca567f3b1d7a100d2d820166d
lukexingdbmgr <lukexingdbmgr@gmail.com>
2017-11-25 23:47:47 -0800

    add one more nicer solution ; add comments

--- LRUCache_O1.py
+++ LRUCache_O1.py
@@ -38,7 +38,7 @@ class LRUCache(object):
         if lnd.prev != self.link_head:
             if self.tail == lnd:
                 self.tail = lnd.prev
-
+            ## make sure it is unlinked
             nd = lnd.unlink()
             first = self.link_head.next
 
@@ -49,7 +49,9 @@ class LRUCache(object):
             if first:
                 first.prev = nd
             else:
+                ## this is the first real element
                 self.tail = nd
+                ##else: as already in the correct position -> do nothing
 
     def get(self, key):
         """
@@ -115,3 +117,53 @@ class LRUCache(object):
                 "link": new_node,
                 "expired": expired if expired else None
             }
+
+
+class Node:
+    def __init__(self, k, v):
+        self.key = k
+        self.val = v
+        self.prev = None
+        self.next = None
+
+
+class LRUCache2:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.dic = dict()
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key):
+        if key in self.dic:
+            n = self.dic[key]
+            self._remove(n)
+            self._add(n)
+            return n.val
+        return -1
+
+    def set(self, key, value):
+        if key in self.dic:
+            self._remove(self.dic[key])
+        n = Node(key, value)
+        self._add(n)
+        self.dic[key] = n
+        if len(self.dic) > self.capacity:
+            n = self.head.next
+            self._remove(n)
+            del self.dic[n.key]
+
+    def _remove(self, node):
+        p = node.prev
+        n = node.next
+        p.next = n
+        n.prev = p
+
+    def _add(self, node):
+        p = self.tail.prev
+        p.next = node
+        self.tail.prev = node
+        node.prev = p
+        node.next = self.tail
