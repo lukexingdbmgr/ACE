803a18d1e14e6419a2bfbe302744bda50e0beb9c
lukexingdbmgr <lukexingdbmgr@gmail.com>
2017-11-29 23:37:38 -0800

    max points on a line fixxing
    using gcd

--- max-points-on-a-line.py
+++ max-points-on-a-line.py
@@ -4,8 +4,6 @@
 #         self.x = a
 #         self.y = b
 
-from collections import defaultdict
-
 
 class Point(object):
     def __init__(self, a=0, b=0):
@@ -13,11 +11,28 @@ class Point(object):
         self.y = b
 
 
+# Definition for a point.
+# class Point:
+#     def __init__(self, a=0, b=0):
+#         self.x = a
+#         self.y = b
+from collections import defaultdict
+
+
 class Solution(object):
+    ## 2# avoid divide operations
+    def gcd(self, x, y):
+        return x if y == 0 else self.gcd(y, x % y)
+
     def getSlop(self, x, y, x1, y1):
         if x == x1:
             return float('inf')
-        return (y - y1) / (x - x1)
+        y = y - y1
+        x = x - x1
+        g = self.gcd(x, y)
+        ## 1# tuple can be hashed
+        return (x / g, y / g)
+        # return float((y - y1) / (x - x1))
 
     def maxPoints(self, points):
         """
@@ -31,15 +46,18 @@ class Solution(object):
         for p in points:
             x, y = p.x, p.y
             ##  key = hash((x, y)) % 90839 is not reliable
-            key = str(x) + "^_^" + str(y) ## use string to give the unique k
-            print(x, y)
-            print(key)
+            key = str(x) + "#" + str(y)  ## use string to give the unique k
             key_tuple[key] = (x, y)
             if num_dict.get(key, None) == None:
                 num_dict[key] = 1
             else:
                 num_dict[key] += 1
 
+        # for k, v in num_dict.items():
+        #    print("key = " , k)
+        #    print("num = ", v)
+
+        # print("-"*101)
         m = 0
         sub = 0
         for k, (x, y) in key_tuple.items():
@@ -53,17 +71,20 @@ class Solution(object):
             sub = 0
             for key in dd:
                 l = dd[key]
-                print(l)
+                # print("k = ", key)
+                # print(l)
                 s = 0
                 for c in l:
                     s += num_dict[c]
                 sub = max(sub, s)
-
+                # print("sub = ", sub)
+            # print("")
             if sub > 0:
+                # print("1:", m, sub+ num_dict[k] )
                 m = max(m, sub + num_dict[k])
             else:
                 m = max(m, num_dict[k])
-
+                # print("2:", m, sub + num_dict[k])
 
         return m
 
@@ -71,8 +92,8 @@ class Solution(object):
 if __name__ == "__main__":
     c = Point(0, 0)
     s = Solution()
-    #l =  [[84,250],[0,0],[1,0],[0,-70],[0,-70],[1,-1],[21,10],[42,90],[-42,-230]]
-    l = [[1,1],[1,1],[1,0],[1,0], [1,-1], [1, -1]]
+    l = [[84, 250], [0, 0], [1, 0], [0, -70], [0, -70], [1, -1], [21, 10], [42, 90], [-42, -230]]
+    # l = [[1,1],[1,1],[1,0],[1,0], [1,-1], [1, -1]]
     fl = []
     for c in l:
         p = Point(c[0], c[1])
