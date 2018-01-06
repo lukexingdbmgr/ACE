e3b3d3bc7e8c024c8a8926aa3d1c2117329a292e
Luke <13213840+lukexingdbmgr@users.noreply.github.com>
2018-01-06 13:06:59 -0800

    second min in bi-tree

---
+++ sum of square numbers.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def judgeSquareSum(self, c):
+        """
+        :type c: int
+        :rtype: bool
+        """
+        if c < 0:
+            return False
+
+        left = 0
+        right = int(c ** (1 / 2.0))
+        while (left <= right):
+            cur = left * left + right * right
+            if cur < c:
+                left += 1
+            elif cur > c:
+                right -= 1
+            else:
+                return True
+
+        return False
+
+
+s = Solution()
+s.judgeSquareSum(5)
