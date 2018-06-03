d04fc98cf8615ba810530dcadcb2ce521b9221ac
lukexingdbmgr <lukexingdbmgr@gmail.com>
2018-06-03 00:05:36 -0700

    second min in bi-tree

--- subSets.py
+++ subSets.py
@@ -20,6 +20,15 @@ def subSets(s):
             res.append(t)
     return res
 
+
+def test_passing(j, l):
+    print(j)
+    for p in range(j, l):
+        test_passing(j + 1, l)
+
+
+# test_passing(0, 3)
+
 def subSets_recur(s, j, res, sub_res):
     print("j = " + str(j))
     res.append(list(sub_res)) ## point 1 need to care
@@ -27,9 +36,15 @@ def subSets_recur(s, j, res, sub_res):
         print(list(range(j, len(s))))
         print("j2 = " + str(j))
         sub_res.append(s[p])
-        # subSets_recur(s, j + 1, res, sub_res)
-        j = j + 1
-        subSets_recur(s, j, res, sub_res)
+        ### here not j + 1;
+        ### subSets_recur(s, j + 1, res, sub_res)
+        ### as for the next loop; it will start from 0 + 1 again
+        ### say j = 0; len(s) = 3
+        ## when p is 0:
+        ## subSets_recur(s, 1, res, sub_res)
+        ## when p is 1:
+        ## still subSets_recur(s, 1, res, sub_res) <--> should be 2 as p + 1
+        subSets_recur(s, p + 1, res, sub_res)
         sub_res.pop()
         print("_____________________________________")
 
@@ -42,15 +57,31 @@ def subSets_recur_2(s, j, res, sub_res):
         print("j2 = " + str(j))
         sub_res.append(s[p])
         # subSets_recur(s, j + 1, res, sub_res)
-        p += 1
-        subSets_recur_2(s, p, res, sub_res)
+
+        subSets_recur_2(s, p + 1, res, sub_res)
         sub_res.pop()
         print("_____________________________________")
+        p += 1
 
+
+def subSets_recur_3(s, j, res, sub_res):
+    print("j = " + str(j))
+    res.append(list(sub_res))  ## point 1 need to care
+    p = j
+    while (p < len(s)):
+        print(list(range(j, len(s))))
+        print("j2 = " + str(j))
+        sub_res.append(s[p])
+        # subSets_recur(s, j + 1, res, sub_res)
+
+        subSets_recur_3(s, p + 1, res, sub_res)
+        sub_res.pop()
+        print("_____________________________________")
+        p += 1
 
 
 res2 = []
-subSets_recur([1,2,3, 10], 0, res2, [])
+subSets_recur([1, 2, 3, 10], 0, res2, [])
 print(res2)
 print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 print(subSets([1,2,3, 10]))
