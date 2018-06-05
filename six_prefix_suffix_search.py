bd0bb83a970df290ba7d725028a6a80f47b76f18
lukexingdbmgr <lukexingdbmgr@gmail.com>
2018-06-04 23:50:08 -0700

    change preffix suffix

--- six_prefix_suffix_search.py
+++ six_prefix_suffix_search.py
@@ -1,26 +1,30 @@
 class WordFilter(object):
-    prefix_d = {}
-    suffix_d = {}
 
-    def gen_prefix(self, word, index):
-        for i in range(1, len(word)):
-            self.prefix_d[word[:i]] = index
+    def gen_data(self, word, index):
+        for i in range(0, len(word) + 1):
+            prefix = word[:i]  ## start from ""
+            for j in range(1, len(word) + 1):
+                suffix = word[-j:]  ## start from "e"
+                self.use_d[prefix + "_" + suffix] = index
+            self.use_d[prefix + "_"] = index
 
-    def gen_suffix(self, word, index):
-        # word = word[::-1]  ## reverse word
-        for i in range(1, len(word)):
-            # self.suffix_d[word[:-i]] = index
-            self.suffix_d[word[-i:]] = index  ## from last i to the end; the above one is wrong
+    def gen_data2(self, word, index):
+        for i in range(0, len(word) + 1):
+            prefix = word[:i]  ## start from ""
+            for j in range(0, len(word) + 1):
+                suffix = word[j:]  ## word[0:] => the whole string
+                self.use_d[prefix + "_" + suffix] = index
 
     def __init__(self, words):
         """
         :type words: List[str]
         """
+        self.use_d = {}  ### !
         p = 0
         while (p < len(words)):
-            self.gen_prefix(words[p], p)
-            self.gen_suffix(words[p], p)
+            self.gen_data2(words[p], p)
             p += 1
+        # print(self.use_d)
 
     def f(self, prefix, suffix):
         """
@@ -29,20 +33,9 @@ class WordFilter(object):
         :rtype: int
         """
 
-        if prefix == "" or suffix == "":
-            return -1
-
-        prefix_match = self.prefix_d.get(prefix, None)
-        suffix_match = self.suffix_d.get(suffix, None)
-        if prefix_match == suffix_match and prefix_match is not None:
-            return prefix_match
-        else:
-            return -1
-
+        key = prefix + "_" + suffix
+        return self.use_d.get(key, -1)
 
 # Your WordFilter object will be instantiated and called as such:
 # obj = WordFilter(words)
 # param_1 = obj.f(prefix,suffix)
-
-c = WordFilter(["apple"])
-print(c.f("", ""))
