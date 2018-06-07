ca1c2034bb5b965339ca487cefd9e876eaaf113e
lukexingdbmgr <lukexingdbmgr@gmail.com>
2018-06-06 23:16:04 -0700

    function ex time 1

--- six_func_ex.py
+++ six_func_ex.py
@@ -1,30 +1,32 @@
-def calculate_logs(logs):
-    res = {}
-    stk = []
-    last_call_time = 0
-    for log in logs:
-        cur_id, cur_status, cur_time = tuple(log.split(':'))
-        if cur_status == 'start':
-            stk.append(log)
-        elif cur_status == 'end':
-            top = stk[-1]
-            top_id, top_status, top_time = tuple(top.split(':'))
-            elapse =  int(cur_time) - int(top_time) - int(last_call_time)
-            res[top_id] = elapse
-            last_call_time = elapse
-            stk.pop()
-    print(res)
+class Solution(object):
+    def exclusiveTime(self, n, logs):
+        """
+        :type n: int
+        :type logs: List[str]
+        :rtype: List[int]
+        """
+        res = {}
+        stk = []
+        last_call_time = 0
+        for log in logs:
+            cur_id, cur_status, cur_time = tuple(log.split(':'))
+            if cur_status == 'start':
+                stk.append(log)
+            elif cur_status == 'end':
+                top = stk[-1]
+                top_id, top_status, top_time = tuple(top.split(':'))
+                elapse = int(cur_time) - int(top_time) - int(last_call_time) + 1
+                res[top_id] = elapse
+                last_call_time += elapse  ## should += not =
+                stk.pop()
+        res_list = []
+        keys = sorted(res)
+        for key in keys:
+            res_list.append(res[key])
+        return res_list
 
 logs = [
-
-'1:start:0',
-'2:start:3',
-
-'3:start:5',
-'3:end:19',
-
-'2:end:24',
-'1:end:90'
+    "0:start:0", "0:start:2", "0:end:5", "0:start:6", "0:end:6", "0:end:7"
 ]
-
-calculate_logs(logs)
+sl = Solution()
+print(sl.exclusiveTime(7, logs))
